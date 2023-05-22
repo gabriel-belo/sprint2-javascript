677cf823ab9f34254c967085d31ae6cf98c2f091
gabriel-belo <rm551669@fiap.com.br>
2023-05-21 21:59:44 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -20,20 +20,6 @@ document.getElementById('lixeiraForm').addEventListener('submit', function(event
     localizacao: localizacaoSelecionada,
     estado: estadoLixeira
   };
-
-  lixeiras.push(lixeiraData); // Armazena a lixeira no array
-
-  // Armazena os dados da lixeira em uma lista
-  const estadoAtualLixeiras = document.getElementById('estadoAtualLixeiras');
-  const lixeiraItem = document.createElement('li');
-  lixeiraItem.textContent = `${lixeiraData.localizacao}: ${lixeiraData.estado}`;
-  estadoAtualLixeiras.appendChild(lixeiraItem);
-
-  // Limpa o formulário
-  document.getElementById('localizacao').value = '';
-  document.getElementById('estadoLixeira').value = '';
-
-  criarCaminho();
 });
 
 function criarCaminho() {
@@ -48,3 +34,40 @@ function criarCaminho() {
     }
   }
 }
+
+function exibirDadosLixeiras() {
+  const estadoAtualLixeiras = document.getElementById('estadoAtualLixeiras');
+  estadoAtualLixeiras.innerHTML = '';
+
+  for (let i = 0; i < lixeiras.length; i++) {
+    const lixeiraItem = document.createElement('li');
+    lixeiraItem.textContent = `${lixeiras[i].localizacao}: ${lixeiras[i].estado}`;
+    estadoAtualLixeiras.appendChild(lixeiraItem);
+  }
+}
+document.getElementById('lixeiraForm').addEventListener('submit', function(event) {
+  event.preventDefault(); // Impede o envio do formulário
+
+  const localizacaoSelecionada = document.getElementById('localizacao').value;
+  const estadoLixeira = document.getElementById('estadoLixeira').value;
+
+  const lixeiraData = {
+    localizacao: localizacaoSelecionada,
+    estado: estadoLixeira
+  };
+
+  lixeiras.push(lixeiraData); // Armazena a lixeira no array
+
+  // Armazena os dados da lixeira em uma lista
+  const estadoAtualLixeiras = document.getElementById('estadoAtualLixeiras');
+  const lixeiraItem = document.createElement('li');
+  lixeiraItem.textContent = `${lixeiraData.localizacao}: ${lixeiraData.estado}`;
+  estadoAtualLixeiras.appendChild(lixeiraItem);
+
+  // Limpa o formulário
+  document.getElementById('localizacao').value = '';
+  document.getElementById('estadoLixeira').value = '';
+
+  criarCaminho();
+  exibirDadosLixeiras();
+});
